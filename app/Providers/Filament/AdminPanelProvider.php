57fe57f56c985062c1b4138ada9f13d7ef63da81
nadiya05 <nadiyazarahislamiati@Nadiyas-MacBook-Pro.local>
2026-02-16 21:13:03 +0700

    style: modifikasi admin panel - tambah foto

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,6 +11,7 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
+use Filament\Navigation\MenuItem;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -58,6 +59,12 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->userMenuItems([
+            'profile' => MenuItem::make()
+                ->label('Pengaturan Akun')
+                ->url(fn () => \App\Filament\Pages\Pengaturan::getUrl())
+                ->icon('heroicon-o-cog-6-tooth'),
+        ]);
     }
 }
